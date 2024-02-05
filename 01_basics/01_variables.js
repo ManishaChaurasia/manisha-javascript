4718e13946e87e8edc82fc0a1145a7ead6177d11
Monica Manisha <93512701+ManishaChaurasia@users.noreply.github.com>
2024-02-05 07:58:29 +0000

    difference let var const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = "Mani09"
+let accountEmail = "manishachaurasia18@gmail.com"
+var accountPassword = "121212"
+accountCity = "jaipur"
+
+console.table([ accountID, accountEmail, accountPassword, accountCity ]);
+
+//accountID = "Monica421"
+accountEmail = "monica.manisha@rubicotech.in"
+accountPassword = "Rubi@123"
+accountCity = "Haridwar"
+
+console.table([ accountEmail, accountPassword, accountCity])
